44eac12ab33aea04a6e5f74979dceb1dd3e2e729
Wing Sang Yau <shawnyauws@gmail.com>
2025-09-15 07:31:06 +0000

    開始著手將應用程式與 Firebase Firestore 和 Storage 連接，以便將資料和圖片儲存到後端

---
+++ src/lib/firebase.ts
@@ -0,0 +1,21 @@
+// src/lib/firebase.ts
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2810n43Lz_l50_MBg2AexFh-a9L9tqNc",
+  authDomain: "bizcard-portfolio-5a025.firebaseapp.com",
+  projectId: "bizcard-portfolio-5a025",
+  storageBucket: "bizcard-portfolio-5a025.appspot.com",
+  messagingSenderId: "367011986503",
+  appId: "1:367011986503:web:71e2b02e737c376174a72d",
+  measurementId: "G-G64GEX84V6"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, db, storage };
