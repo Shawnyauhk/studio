b5cd00992736866fd504c252d97dbd35c4ec2552
Wing Sang Yau <shawnyauws@gmail.com>
2025-09-15 08:25:24 +0000

    gourmet-ai-yfnzg是另一個專案,與這個無關

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,9 +5,9 @@ import { getStorage } from 'firebase/storage';
 
 const firebaseConfig: FirebaseOptions = {
   apiKey: "AIzaSyDBp4ux24hmjr-5nP9u5RGV-m4h2oLkZoY",
-  authDomain: "gourmet-ai-yfnzg.firebaseapp.com",
-  projectId: "gourmet-ai-yfnzg",
-  storageBucket: "gourmet-ai-yfnzg.appspot.com",
+  authDomain: "studio-697601028-76b1c.firebaseapp.com",
+  projectId: "studio-697601028-76b1c",
+  storageBucket: "studio-697601028-76b1c.appspot.com",
   messagingSenderId: "60808022828",
   appId: "1:60808022828:web:9cd0358fa9c9cca4e27a89"
 };
